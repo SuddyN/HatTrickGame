299a5cd9d588f8caa621f72774ca37f4f3e6743c
ncsuddut <ncsuddut@ncsu.edu>
2022-03-30 21:58:12 -0400

    re-implement the rocket jump

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,7 +6,9 @@ public class CharacterController: MonoBehaviour {
 
     public float movementSpeed = 10;
     public float jumpStrength = 10;
+    public float fireStrength = 10;
     public float cursorDist = 1;
+    public bool usingController = false;
     public GameObject cursor;
     private Rigidbody2D _rigidbody2D;
 
@@ -23,11 +25,27 @@ public class CharacterController: MonoBehaviour {
             _rigidbody2D.AddForce(new Vector2(0, jumpStrength), ForceMode2D.Impulse);
         }
 
-        Vector2 aim = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector2 aim = new Vector2(mousePos.x, mousePos.y).normalized;
+        if (usingController) {
+            aim = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        }
         if (aim.magnitude > cursorDist) {
             aim = aim.normalized * cursorDist;
         }
         Vector3 cursorPos = new Vector3(transform.position.x + aim.x, transform.position.y + aim.y, transform.position.z);
         cursor.transform.position = cursorPos;
+
+        if (Input.GetButtonDown("Fire1")) {
+            Fire(cursorPos);
+        }
+    }
+
+    private void Fire(Vector3 cursorPos) {
+        Vector2 aimVector = new Vector3(cursorPos.x - transform.position.x, cursorPos.y - transform.position.y);
+        if (aimVector.magnitude < 0.001f) {
+            return;
+        }
+        _rigidbody2D.velocity = _rigidbody2D.velocity + (aimVector * fireStrength * -1.0f);
     }
 }
