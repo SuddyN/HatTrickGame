864ce0f59034755e4b5c324328a03918b9381c50
SuddyN <ncsuddut@ncsu.edu>
2022-04-17 21:48:33 -0400

    Rewrite CameraFollow Script

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -22,39 +22,40 @@ public class CharacterController: MonoBehaviour {
     }
 
     void Update() {
-        if (GameManager.Instance.gameState.Equals(GameState.Game)) {
-            // Only allowed to jump or move from the ground
-            if (Mathf.Abs(_rigidbody2D.velocity.y) < 0.001f) {
+        if (!GameManager.Instance.gameState.Equals(GameState.Game)) {
+            return;
+        }
+        // Only allowed to jump or move from the ground
+        if (Mathf.Abs(_rigidbody2D.velocity.y) < 0.001f) {
 
-                // Get the user inputted movement
-                float maxSpeed = Input.GetAxisRaw("Horizontal") * movementSpeed;
-                // Add it to the current velocity
-                Vector2 newVelocity = new Vector2(_rigidbody2D.velocity.x + (maxSpeed * movementAccel * Time.deltaTime), _rigidbody2D.velocity.y);
-                // Only apply new velocity if it would be less than max speed
-                if (Mathf.Abs(newVelocity.x) < movementSpeed) {
-                    _rigidbody2D.velocity = newVelocity;
-                }
-
-                // Handle jumping
-                if (Input.GetButtonDown("Jump")) {
-                    _rigidbody2D.AddForce(new Vector2(0, jumpStrength), ForceMode2D.Impulse);
-                }
+            // Get the user inputted movement
+            float maxSpeed = Input.GetAxisRaw("Horizontal") * movementSpeed;
+            // Add it to the current velocity
+            Vector2 newVelocity = new Vector2(_rigidbody2D.velocity.x + (maxSpeed * movementAccel * Time.deltaTime), _rigidbody2D.velocity.y);
+            // Only apply new velocity if it would be less than max speed
+            if (Mathf.Abs(newVelocity.x) < movementSpeed) {
+                _rigidbody2D.velocity = newVelocity;
             }
 
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            Vector2 aim = new Vector2(mousePos.x, mousePos.y).normalized;
-            if (usingController) {
-                aim = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            // Handle jumping
+            if (Input.GetButtonDown("Jump")) {
+                _rigidbody2D.AddForce(new Vector2(0, jumpStrength), ForceMode2D.Impulse);
             }
-            if (aim.magnitude > cursorDist) {
-                aim = aim.normalized * cursorDist;
-            }
-            Vector3 cursorPos = new Vector3(transform.position.x + aim.x, transform.position.y + aim.y, transform.position.z);
-            cursor.transform.position = cursorPos;
+        }
 
-            if (Input.GetButtonDown("Fire1")) {
-                Fire(cursorPos);
-            }
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector2 aim = new Vector2(mousePos.x, mousePos.y).normalized;
+        if (usingController) {
+            aim = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        }
+        if (aim.magnitude > cursorDist) {
+            aim = aim.normalized * cursorDist;
+        }
+        Vector3 cursorPos = new Vector3(transform.position.x + aim.x, transform.position.y + aim.y, transform.position.z);
+        cursor.transform.position = cursorPos;
+
+        if (Input.GetButtonDown("Fire1")) {
+            Fire(cursorPos);
         }
     }
 
